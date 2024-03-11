817a73665256292bd7f02bdc7548ad58f030e4f5
shubamjagtap <smjdevopservices@gmail.com>
2024-03-11 22:52:32 +0530

    added register frontend and register backend

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+const app = express();
+
+app.get('/test', (req, res) => {
+    res.json('test ok');
+});
+
+app.post('/register', (req, res) => {
+
+});
+
+app.listen(4000);
